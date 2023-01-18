a9148824499111a1dc6c6d49862da3dfeea000f8
tripathirahul274 <tripathirahul274@gmail.com>
2023-01-18 17:28:49 +0530

    homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,345 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+
+
+
+        <div id="first_section" onclick="gotomen()">
+          <div className="frist_section_img sameimgsamdiv">
+            <img src="./Images/fiest section.jpg" alt="" />
+          </div>
+        </div>
+        <div id="Second_section" onclick="gotowoen()">
+          <div className="Second_section_img sameimgsamdiv">
+            <img src="./Images/second section.jpg" alt="" />
+          </div>
+        </div>
+        <div id="Third_section" onclick="gotowoen()">
+          <div className="Third_section_img sameimgsamdiv">
+            <img src="./Images/third section.jpg" alt="" />
+          </div>
+        </div>
+        <div id="Four_section" onclick="gotomen()">
+          <div className="Four_section_img sameimgsamdiv">
+            <img src="./Images/Fous section/1.jpg" alt="" />
+          </div>
+          <div className="Four_section_img sameimgsamdiv">
+            <img src="./Images/Fous section/2.jpg" alt="" />
+          </div>
+        </div>
+        <div className="sameimgsamdiv sameimgheading" onclick="gotomen()" />
+        <div id="sixth_silder" onclick="gotowoen()">
+          <div className="Sixth_silder_div single-item">
+            <div className="silder_no_hover">
+              <img src="./Images/sic the section slider/1.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/sic the section slider/2.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/sic the section slider/3.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/sic the section slider/4.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/sic the section slider/5.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/sic the section slider/6.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="sameimgsamdiv sameimgheading" onclick="gotowoen()" />
+        <div id="Eight_silder " onclick="gotomen()">
+          <div className="Eight_silder_div single-item">
+            <div className="silder_no_hover">
+              <img src="./Images/eight Section Silder/1.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/eight Section Silder/2.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/eight Section Silder/3.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="sameimgsamdiv sameimgheading" onclick="gotowoen()" />
+        <div id="Ten_silder" onclick="gotowoen()">
+          <div className="Sixth_silder_div Ten">
+            <div className="silder_no_hover">
+              <img src="./Images/tenth Section Silder/1.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/tenth Section Silder/2.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/tenth Section Silder/3.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/tenth Section Silder/4.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/tenth Section Silder/5.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/tenth Section Silder/6.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="Show_the_space" onclick="gotomen()" />
+        <div className="sameimgsamdiv sameimgheading" onclick="gotowoen()" />
+        <div id="section_11" className="Fous_img_inDiv" onclick="gotomen()">
+          <div className="section_11_img sameimgsamdiv">
+            <img src="./Images/11 Section/1.webp" alt="" />
+          </div>
+          <div className="section_11_img sameimgsamdiv">
+            <img src="./Images/11 Section/2.webp" alt="" />
+          </div>
+          <div className="section_11_img sameimgsamdiv">
+            <img src="./Images/11 Section/3.webp" alt="" />
+          </div>
+          <div className="section_11_img sameimgsamdiv">
+            <img src="./Images/11 Section/4.webp" alt="" />
+          </div>
+        </div>
+        <div className="Show_the_space" onclick="gotowoen()" />
+        <div className="sameimgsamdiv sameimgheading" onclick="gotowoen()" />
+        <div id="section12" onclick="gotowoen()">
+          <div className="section12 sameimgsamdiv">
+            <img src="./Images/12.webp" alt="" />
+          </div>
+        </div>
+        <div className="Show_the_space" onclick="gotomen()" />
+        <div className="sameimgsamdiv sameimgheading" onclick="gotowoen()" />
+        <div id="section_13 " className="Fous_img_inDiv" onclick="gotowoen()">
+          <div className="section_13_img sameimgsamdiv">
+            <img src="./Images/13section/1.jpg" alt="" />
+          </div>
+          <div className="section_13_img sameimgsamdiv">
+            <img src="./Images/13section/2.webp" alt="" />
+          </div>
+          <div className="section_13_img sameimgsamdiv">
+            <img src="./Images/13section/3.webp" alt="" />
+          </div>
+          <div className="section_13_img sameimgsamdiv">
+            <img src="./Images/13section/4.webp" alt="" />
+          </div>
+        </div>
+        <div className="Show_the_space" onclick="gotomen()" />
+        <div className="sameimgsamdiv sameimgheading" onclick="gotowoen()" />
+        <div id="silder_14_section" onclick="gotomen()">
+          <div className="silder_14_section_silder_div Ten">
+            <div className="silder_no_hover">
+              <img src="./Images/14Section Img Silder/1.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/14Section Img Silder/2.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/14Section Img Silder/3.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/14Section Img Silder/4.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/14Section Img Silder/5.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/14Section Img Silder/6.webp" alt="" />
+            </div><div className="silder_no_hover">
+              <img src="./Images/14Section Img Silder/7.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="Show_the_space" onclick="gotomen()" />
+        <div className="sameimgsamdiv sameimgheading" onclick="gotowoen()" />
+        <div id="section15" onclick="gotomen()">
+          <div className="section15 sameimgsamdiv">
+            <img src="./Images/15section img/1.webp" alt="" />
+          </div>
+          <div className="section15 sameimgsamdiv">
+            <img src="./Images/15section img/2.webp" alt="" />
+          </div>
+        </div>
+        <div className="Show_the_space" />
+        <div className="sameimgsamdiv sameimgheading" />
+        <div id="section_16" className="arrowsSilder" onclick="gotowoen()">
+          <div className="Section_16_silder Section_arrows_silder responsive">
+            <div className="silder_no_hover">
+              <img src="./Images/16 Secion silder img/1.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/16 Secion silder img/2.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/16 Secion silder img/3.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/16 Secion silder img/4.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/16 Secion silder img/5.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/16 Secion silder img/6.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/16 Secion silder img/7.webp" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="Show_the_space" onclick="gotomen()" />
+        <div className="sameimgsamdiv sameimgheading" />
+        <div id="section_17" onclick="gotomen()">
+          <div className="section_17 sameimgsamdiv">
+            <img src="./Images/17 section img.webp" alt="" />
+          </div>
+        </div>
+        <div id="section__17" className="arrowsSilder" onclick="gotomen()">
+          <div className="Section_16_silder Section_arrows_silder responsive">
+            <div className="silder_no_hover">
+              <img src="./Images/17 sectin silder/1.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/17 sectin silder/2.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/17 sectin silder/3.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/17 sectin silder/4.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/17 sectin silder/5.webp" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="Show_the_space" onclick="gotomen()" />
+        <div className="sameimgsamdiv sameimgheading" />
+        <div id="section18">
+          <div className="section18 sameimgsamdiv">
+            <img src="./Images/18.webp" alt="" />
+          </div>
+        </div>
+        <div id="section_18 " className="Fous_img_inDiv">
+          <div className="section_18_img sameimgsamdiv">
+            <img src="./Images/18section 4/1.jpg" alt="" />
+          </div>
+          <div className="section_18_img sameimgsamdiv">
+            <img src="./Images/18section 4/2.jpg" alt="" />
+          </div>
+          <div className="section_18_img sameimgsamdiv">
+            <img src="./Images/18section 4/3.webp" alt="" />
+          </div>
+          <div className="section_18_img sameimgsamdiv">
+            <img src="./Images/18section 4/4.webp" alt="" />
+          </div>
+        </div>
+        <div className="Show_the_space" onclick="gotomen()" />
+        <div className="sameimgsamdiv sameimgheading" onclick="gotowoen()" />
+        <div id="section_19" onclick="gotomen()">
+          <div className="section_19 Ten">
+            <div className="silder_no_hover">
+              <img src="./Images/19  section slider/1.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/19  section slider/2.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/19  section slider/3.jpg" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images/19  section slider/4.webp" alt="" />
+            </div>
+          </div>
+        </div>
+        <div id="section__19" className="arrowsSilder" onclick="gotowoen()">
+          <div className="Section_19_silder Section_arrows_silder responsive">
+            <div className="silder_no_hover">
+              <img src="./Images/20 sectio slider/1.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images//20 sectio slider/2.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images//20 sectio slider/3.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images//20 sectio slider/4.webp" alt="" />
+            </div>
+            <div className="silder_no_hover">
+              <img src="./Images//20 sectio slider/5.webp" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="Show_the_space" onclick="gotomen()" />
+        <div className="sameimgsamdiv sameimgheading" />
+        <div id="section21" onclick="gotomen()">
+          <div className="section18 sameimgsamdiv">
+            <img src="./Images/21.webp" alt="" />
+          </div>
+        </div>
+        <div className="section_21" onclick="gotomen()">
+          <div className="sameimgsamdiv">
+            <img src="./Images/lastsection22/1.webp" alt="" />
+          </div>
+          <div className="sameimgsamdiv">
+            <img src="./Images/lastsection22/2.webp" alt="" />
+          </div>
+          <div className="sameimgsamdiv">
+            <img src="./Images/lastsection22/3.webp" alt="" />
+          </div>
+        </div>
+        <div className="section_21" onclick="gotomen()">
+          <div className="section_13_img sameimgsamdiv">
+            <img src="./Images/lastsection22/4.webp" alt="" />
+          </div>
+          <div className="section_13_img sameimgsamdiv">
+            <img src="./Images/lastsection22/5.webp" alt="" />
+          </div>
+          <div className="section_13_img sameimgsamdiv">
+            <img src="./Images/lastsection22/6.webp" alt="" />
+          </div>
+        </div>
+        <div className="section_21" onclick="gotowoen()">
+          <div className="section_13_img sameimgsamdiv">
+            <img src="./Images/lastsection22/7.webp" alt="" />
+          </div>
+          <div className="section_13_img sameimgsamdiv">
+            <img src="./Images/lastsection22/8.webp" alt="" />
+          </div>
+          <div className="section_13_img sameimgsamdiv">
+            <img src="./Images/lastsection22/9.webp" alt="" />
+          </div>
+          <div className="section_13_img sameimgsamdiv">
+            <img src="./Images/lastsection22/10.webp" alt="" />
+          </div>
+        </div>
+        <div className="Show_the_space" onclick="gotomen()" />
+        <div id="section23" onclick="gotomen()">
+          <div className="section23 sameimgsamdiv">
+            <img src="./Images/last img.webp" alt="" />
+          </div>
+        </div>
+        <div id="icon_div">
+          <div className="icons">
+            <i className="fas fa-shopping-bag" />
+            <p>EASY EXCHANGE</p>
+          </div>
+          <div className="icons">
+            <i className="fas fa-hand-holding-heart" />
+            <p>100% HANDPICKED</p>
+          </div>
+          <div className="icons">
+            <i className="far fa-check-circle" />
+            <p>ASSURED QUALITY</p>
+          </div>
+        </div>
+        <footer>
+        </footer>
+      </div>
   );
 }
 
